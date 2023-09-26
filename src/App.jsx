918b414735ec8db207bd3970ea92d5889b237b09
Donald Reddy I <donaldreddy2712@gmail.com>
2023-09-26 21:31:32 +0530

    update UI

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,40 @@ import useCurrencyInfo from "./hooks/useCurrencyInfo.js";
 
 function App() {
 
-  let currencies = useCurrencyInfo("usd")
-
-  let currenciesList = Object.keys(currencies);
-
   let [from, setFrom] = React.useState(0);
   let [to, setTo] = React.useState(0);
   let [fromCurrency, setFromCurrency] = React.useState("usd");
   let [toCurrency, setToCurrency] = React.useState("inr");
 
+  let currencies = useCurrencyInfo(fromCurrency);
+
+  let currenciesList = Object.keys(currencies);
+
   function swap() {
     setFromCurrency(toCurrency);
     setToCurrency(fromCurrency);
+    setFrom(to);
+    setTo(from);
   }
 
   function convert() {
     setTo(from * currencies[toCurrency]);
+    console.log(fromCurrency, from, currencies[fromCurrency], toCurrency, to, currencies[toCurrency], from * currencies[toCurrency]);
   }
 
   return (
     <div className="container">
-      <InputBox label="from" amount={from} selectedCurrency={fromCurrency} onAmountChange={setFrom} onCurrencyChange={setFromCurrency} currenciesList={currenciesList}></InputBox>
-      <button className="swap" onClick={swap}>Swap</button>
-      <InputBox label="to" amount={to} selectedCurrency={toCurrency} onCurrencyChange={setToCurrency} currenciesList={currenciesList}></InputBox>
-      <button onClick={convert}>Convert {fromCurrency.toUpperCase()} to {toCurrency.toUpperCase()} </button>
+
+      <h1>Currency Converter 💸</h1>
+
+      <InputBox label="From" amount={from} selectedCurrency={fromCurrency} onAmountChange={setFrom} onCurrencyChange={setFromCurrency} currenciesList={currenciesList}></InputBox>
+
+      <button className="swap" onClick={swap}>Swap ⬆⬇</button>
+
+      <InputBox label="To" amount={to} selectedCurrency={toCurrency} onAmountChange={setTo} onCurrencyChange={setToCurrency} isReadOnly={true} currenciesList={currenciesList}></InputBox>
+
+      <button className="convert" onClick={convert}>Convert {fromCurrency.toUpperCase()} to {toCurrency.toUpperCase()} </button>
+
     </div>
   )
 }
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import "./InputBox.css"
 
-function InputBox({ label, amount, selectedCurrency, onAmountChange, onCurrencyChange, currenciesList = [] }) {
+function InputBox({ label, amount, selectedCurrency, onAmountChange, onCurrencyChange, currenciesList = [], isReadOnly = false }) {
     return (
         <div className="input-box">
             <div className="box-1">
                 <label>
                     {label}
                 </label>
-                <input type="number" min={0} value={amount} placeholder="Amount" onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))} />
+                <input type="number" min={0} value={amount} placeholder="Amount" readOnly={isReadOnly} onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))} />
             </div>
 
             <div className="box-2">
-                <p>Currency Type</p>
-                <select value={selectedCurrency} onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}>
+                <label htmlFor="currency-selector" >Currency Type</label>
+                <select name="currency-selector" value={selectedCurrency} onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}>
 
                     {currenciesList.map((currency) => (
                         <option key={currency} value={currency}>
